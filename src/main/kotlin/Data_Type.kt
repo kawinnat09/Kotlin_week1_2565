840912e8d19f638c5d3806eaf2a4ab4bbceb489c
kawinnat09 <Kwinnathmayura@gmail.com>
2022-10-20 13:49:42 +0700

    first commit

---
+++ src/main/kotlin/Data_Type.kt
@@ -0,0 +1,29 @@
+fun kDataType(){
+    println("Hello World")
+    println("#Signed Intergral Type \n ==============")
+    val kByte:UByte = 255_u
+    val kShort:UShort = 65_535u
+    println("kByte = $kByte")
+    println("kShort = $kShort")
+}
+
+fun kDataType2(){
+    println("#Signed Intergral Type \n ==============")
+    val kInt:Int = 2_147_483_647
+    val kLong:Long = 9_223_372_036_854_775_807L
+    println(kInt)
+    println(kLong)
+}
+
+fun kDataType3(){
+    println("#Bulean Type \n ==============")
+    val kBulean1: Boolean = true
+    print("Mark = $kBulean1")
+}
+fun kDataType4(){
+    println("#Floating point Type \n ==============")
+    val kFloat: Float = 3.14F
+    val kDouble: Double = 1.79
+    println("KFloating = $kFloat")
+    println("KFloating = $kDouble")
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    kDataType()
+
+    println("#Any Type \n ==============")
+    val kAny1: Any = 20
+    val kAny2: Any = 100.4
+    val kAny3: Any = "Mark Zuckerberg"
+    println("kAny1: $kAny1")
+    println("kAny2: $kAny2")
+    println("kAny3: $kAny3")
+
+//    var a = 10 ; var b = 5 ; var c = a+b
+//    println(c)
+//    println("Hello Kotlin")
+//    println("สวัสดีชาวเอสโมกี")
+//    println(4850+1600)
+//    println("ค่าเทอมอย่างแพง พัดลมแรงๆก็ไม่มี")
+}
